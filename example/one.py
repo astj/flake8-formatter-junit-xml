f22932a7e4241bbd1e314cf300283805e3607169
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-12-14 18:03:30 +0900

    implement

---
+++ example/one.py
@@ -0,0 +1,3 @@
+def one():
+    # Will cause "E111 indentation is not a multiple of four"
+     pass
---
+++ flake8_formatting_junit.py
@@ -0,0 +1,34 @@
+from flake8.formatting import base
+from junit_xml import TestSuite, TestCase
+
+
+class JUnitFormatter(base.BaseFormatter):
+    """JUnit formatter for Flake8."""
+
+    test_suites = dict()
+
+    def beginning(self, filename):
+        name = '{0}.{1}'.format("flake8", filename.replace('.', '_'))
+        self.test_suites[filename] = TestSuite(name, file=filename)
+
+    def format(self, error):
+        name = '{0}, {1}'.format(error.code, error.text)
+        test_case = TestCase(name, file=error.filename, line=error.line_number)
+        message = '%(path)s:%(row)d:%(col)d: %(code)s %(text)s' % {
+            "code": error.code,
+            "text": error.text,
+            "path": error.filename,
+            "row": error.line_number,
+            "col": error.column_number,
+        }
+        test_case.add_failure_info(message)
+        self.test_suites[error.filename].test_cases.append(test_case)
+
+    # Do not write each error
+    def write(self, line, source):
+        pass
+
+    # writes results to file after all files are processed
+    def stop(self):
+        self._write(TestSuite.to_xml_string(iter(self.test_suites.values())))
+        super().stop()
