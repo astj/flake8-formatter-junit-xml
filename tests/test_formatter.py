6b067c23fd45b07b61dd78d7f13c610ed3930c86
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-12-28 11:49:42 +0900

    implement tests

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -1,6 +1,49 @@
 import unittest
+import optparse
+import os
+import tempfile
 from flake8_formatter_junit_xml import JUnitXmlFormatter
+from flake8 import style_guide
+from junit_xml import TestSuite, TestCase
+
+filename = 'some/filename.py'
+error = style_guide.Violation('A000', filename, 2, 1, 'wrong wrong wrong', 'import os')
+
+
+def create_formatter(**kwargs):
+    kwargs.setdefault('output_file', None)
+    kwargs.setdefault('tee', False)
+    return JUnitXmlFormatter(optparse.Values(kwargs))
+
 
 class TestJunitXmlFormatter(unittest.TestCase):
-    def test_junit_xml_formatter(self):
-        pass
+
+    def test_beginning(self):
+        f = create_formatter()
+        f.beginning(filename)
+        self.assertIsInstance(f.test_suites[filename], TestSuite)
+        self.assertEqual('flake8.some/filename_py', f.test_suites[filename].name)
+
+    def test_handle(self):
+        f = create_formatter()
+        f.beginning(filename)
+        f.handle(error)
+        self.assertIsNotNone(f.test_suites[filename].test_cases)
+        self.assertIsInstance(f.test_suites[filename].test_cases[0], TestCase)
+
+    def test_scenario(self):
+        (fd, tempfilename) = tempfile.mkstemp()
+
+        with open(os.path.dirname(os.path.abspath(__file__)) + '/expected.xml', 'r') as f:
+            xml_content = f.read(None)
+
+        formatter = create_formatter(output_file=tempfilename)
+        formatter.start()
+        formatter.beginning(filename)
+        formatter.handle(error)
+        formatter.finished(filename)
+        formatter.stop()
+
+        with os.fdopen(fd) as f:
+            content = f.read(None)
+            self.assertEqual(xml_content, content)
