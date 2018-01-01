df1d9c2223ca447a481dc83027df46b2b6e4e58c
Asato Wakisaka <asato.wakisaka@gmail.com>
2018-01-01 12:22:02 +0900

    sort suites

--- flake8_formatter_junit_xml/formatter.py
+++ flake8_formatter_junit_xml/formatter.py
@@ -32,7 +32,11 @@ class JUnitXmlFormatter(base.BaseFormatter):
             dummy_case = TestCase("Check passed", file=filename)
             self.test_suites[filename].test_cases.append(dummy_case)
 
+    # sort to generate a stable output
+    def sorted_suites(self):
+        return map(lambda x: x[1], sorted(self.test_suites.items()))
+
     # writes results to file after all files are processed
     def stop(self):
-        self._write(TestSuite.to_xml_string(iter(self.test_suites.values())))
+        self._write(TestSuite.to_xml_string(iter(self.sorted_suites())))
         super(JUnitXmlFormatter, self).stop()
